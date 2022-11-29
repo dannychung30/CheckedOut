899348e0beb360ad04ebb3dbea8553b120f29b73
Danny Chung <dannychung6158@gmail.com>
2022-11-28 16:36:47 -0800

    added database functionality

--- php/thank_you_page.php
+++ php/thank_you_page.php
@@ -9,6 +9,28 @@
     <link rel="stylesheet" href="../css/thank_you_page.css">
 </head>
 <body>
+<?php
+    // checking user's input
+    if ($_GET['create-password'] != $_GET['confirm-password']) {
+        exit("<p>Your passwords did not match. Go back to the previous page");
+    }
+
+    // connecting with database
+    require_once("config.php");
+
+    // required table info
+    $table_name = "";
+    $first_name = $_GET['first-name'];
+    $last_name = $_GET['last-name'];
+    $email = $_GET['email'];
+    $password = $_GET['create-password'];
+
+    // check to see if this email is already registered
+
+
+    // close connection 
+    $pdo = null;
+?>
     <div id="main-div">
         <h1>Thank you for signing up!</h1>
         <h3><a href="login.html">Click here to login.</a></h3>
