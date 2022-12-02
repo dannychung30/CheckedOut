a0a402cbbef256f572f95621d5f6a630fa2716ab
Danny Chung <dannychung6158@gmail.com>
2022-12-02 13:06:27 -0800

    changed to php file

---
+++ php/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sign In</title>
+  <link rel="stylesheet" href="../css/login.css">
+  <link rel="stylesheet" href="../css/global_style.css">
+</head>
+
+<body>
+
+  <div id="main-div">
+    <h1>Login</h1>
+    <div id="secondary-div">
+      <form>
+        <input type="email" id="email-address-login" name="email" placeholder="Email Address" required><br /><br />
+        <input type="password" id="password-login" name="password" placeholder="Password" required><br /><br />
+        <button id="login-button">Log in</button>
+      </form>
+      <p>Forgot your password?</p>
+      <p><a href="../php/new_customer_signup.php">Create an account</a></p>
+      <p><a href="Homepage.html">&#8592; Back to home</a></p>
+    </div>
+  </div>
+
+</body>
+
+</html>
