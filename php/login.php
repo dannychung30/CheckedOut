0998b6ae542f689d17419a173b78aa93db2356f3
Danny Chung <dannychung6158@gmail.com>
2022-12-02 16:19:04 -0800

    added sessionn functionality

--- php/login.php
+++ php/login.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+// sesssion variables
+$email = $_SESSION['email'];
+$first_name = $_SESSION['first_name'];
+$last_name = $_SESSION['last_name'];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
