31ee98253e5c2fb49479252024a949de076aa51e
Danny Chung <dannychung6158@gmail.com>
2022-12-04 18:46:56 -0800

    initial commit

---
+++ js/Menu.js
@@ -0,0 +1,16 @@
+function addToCart() {
+  const menu = document.getElementById('menu');
+  const inputs = menu.getElementsByTagName('input');
+
+  const selectedItems = Array.from(inputs)
+    .filter((item) => item.checked)
+    .map((item) => {
+      return { name: item.name, price: item.value };
+    });
+
+  window.localStorage.setItem('cart', '[]');
+  window.localStorage.setItem('cart', JSON.stringify(selectedItems));
+}
+
+const add_to_cart_btn = document.getElementById('add_to_cart_btn');
+add_to_cart_btn.addEventListener('click', addToCart);
